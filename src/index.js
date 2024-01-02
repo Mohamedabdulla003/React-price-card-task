c65ca1a32cca60d8c5f5b6b2c6b618715bed23ab
Mohamedabdulla003 <abdullamohamed406@gmail.com>
2024-01-02 17:42:55 +0530

    src/index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-function Pricecard() {
-  return (
-    <div>
-    <h1>Pricecard</h1>
-    </div>
-  )
-}
-
-export default Pricecard
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
